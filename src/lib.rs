0c11cfbf8a2ce3ab77bb920508199eaddcf6f58c
Michael W <michael@developerwakeling.com>
2019-01-13 16:20:56 -0500

    Added test for search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,8 @@ use std::io::prelude::*;
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename)?;
-
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
-
-    println!("{}", config.query);
-    println!("With text \n {}", contents);
     Ok(())
 }
 
@@ -27,5 +23,31 @@ impl Config {
         let query = args[1].clone(); //Not the most efficient way to copy a string
         let filename = args[2].clone();
         Ok(Config { query, filename })
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    //Iterate through each line of contents
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+//Tests...
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
