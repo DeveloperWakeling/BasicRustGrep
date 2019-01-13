cfe08123ad21eb0ee9a45a1ccbed1cf2963cb2b8
Michael W <michael@developerwakeling.com>
2019-01-13 15:33:10 -0500

    Error handling all around

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env; //Needed for accessing the environment args
+use std::error::Error;
+use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -7,14 +9,25 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     //Need logic check to make sure there are the correct number of arguments supplied
-    let config = Config::new(&args); //Technically could give up ownership here???
-    let mut f = File::open(config.filename).expect("File not found");
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    }); //Unwrap or else is basically like let if, except it handles the Err and sets variable to Ok inner
+    if let Err(e) = run(config) {
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("Error reading file");
+    f.read_to_string(&mut contents)?;
 
     println!("{}", config.query);
     println!("With text \n {}", contents);
+    Ok(())
 }
 
 struct Config {
@@ -23,9 +36,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if(args.len() < 3){
+            return Err("Not Enough Arguments");
+        }
         let query = args[1].clone(); //Not the most efficient way to copy a string
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
